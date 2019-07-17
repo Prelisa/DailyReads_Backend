2acb30394e02eaa9de11ef8e392bd572d65eaf31
prelisa <dprelisa@gmail.com>
2019-07-18 00:52:42 +0545

    day2

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const fs= require('fs');
 
+
+//BLOCKING,SYNCHRONOUS
+
 const textin=fs.readFileSync('./starter/txt/input.txt','utf-8');
 console.log(textin);
 
 const textout= `THis is what we know about the avocado ${textin}.\n Created in ${Date.now()}`;
 fs.writeFileSync('./starter/txt/output.txt',textout);
 console.log('file written');
+
+
+//NOn blocking asynchronous
+
+fs.readFile('./starter/txt/start.txt','utf-8',(err,data1)=>{
+    fs.readFile(`./starter/txt/${data1}`,'utf-8',(err,data2)=>{
+        console.log(data2);
+    });
+
+});
+console.log("read this");
